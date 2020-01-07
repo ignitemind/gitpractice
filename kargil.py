c2b356ae9af7d3c85b276cfb34acc924c388a810
Lokesh069 <seagulls069@gmail.com>
2020-01-07 13:59:50 +0530

    updated 6.0 kargil

--- kargil.py
+++ kargil.py
@@ -5,3 +5,5 @@ many soldiers died in kargil
 kargil is a place of uncertainity
 kargil war is between india and pakistan
 kargil war is lost by pak
+kargil soldiers are very brave
+kargil war outbroke due to border conflict
